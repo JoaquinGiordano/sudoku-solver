c3b7132eab90a4c63df9c6cbbe1f7c8f16136623
Joaquin Giordano <joaquin_giordano@outlook.com>
2024-12-22 01:05:25 -0300

    Solving verification bug

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 class Sudoku {
-  constructor(container, resolve_button, size = 3) {
+  constructor(container, resolve_button) {
     this.container = container;
-    this.size = Math.max(3, size);
+
     this.board = [];
     this.#load_matrix();
     this.#render_sudoku();
     if (resolve_button) {
       resolve_button.addEventListener('click', () => {
+        if (!this.#is_board_valid()) return alert('Sudoku Invalido');
         this.solve(0, 0);
         this.#render_sudoku();
       });
@@ -38,12 +39,28 @@ class Sudoku {
     return this.#is_valid_row(row, num) && this.#is_valid_column(col, num) && this.#is_valid_subgrid(row, col, num);
   }
 
+  #is_board_valid() {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        const num = this.board[i][j];
+
+        if (num !== 0) {
+          this.board[i][j] = 0;
+          const is_valid = this.is_valid(i, j, num);
+          this.board[i][j] = num;
+          if (!is_valid) return false;
+        }
+      }
+    }
+    return true;
+  }
+
   solve(row = 0, col = 0) {
-    if (row == this.size * 3) return true;
-    if (col == this.size * 3) return this.solve(row + 1, 0);
+    if (row == 9) return true;
+    if (col == 9) return this.solve(row + 1, 0);
     if (this.board[row][col] != 0) return this.solve(row, col + 1);
 
-    for (let num = 1; num <= this.size * 3; num++) {
+    for (let num = 1; num <= 9; num++) {
       if (this.is_valid(row, col, num)) {
         this.board[row][col] = num;
         if (this.solve(row, col + 1)) {
@@ -56,19 +73,19 @@ class Sudoku {
   }
 
   #load_matrix() {
-    this.board = Array(3 * this.size)
+    this.board = Array(9)
       .fill()
-      .map(() => Array(3 * this.size).fill(0));
+      .map(() => Array(9).fill(0));
   }
 
   #render_sudoku() {
     this.container.innerHTML = '';
-    const aux_matrix = Array(3 * this.size)
+    const aux_matrix = Array(9)
       .fill()
-      .map(() => Array(3 * this.size).fill(0));
+      .map(() => Array(9).fill(0));
 
-    for (let i = 0; i < this.size * 3; i++) {
-      for (let j = 0; j < this.size * 3; j++) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
         const input = document.createElement('input');
         input.id = `sudoku-input-${i}.${j}`;
         input.classList.add('sudoku-cell', `sudoku-col-${j}`);
@@ -88,7 +105,7 @@ class Sudoku {
         aux_matrix[i][j] = input;
       }
     }
-    for (let i = 0; i < this.size * 3; i++) {
+    for (let i = 0; i < 9; i++) {
       const row = document.createElement('div');
       row.id = `sudoku-row-${i}`;
       row.classList.add('sudoku-row');
